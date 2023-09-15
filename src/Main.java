f97f77cd00a7c217b32c7ee0516777db29d0d248
jesusc0208 <jesuscastillo0208@hotmail.com>
2023-09-15 13:58:44 -0700

    added nedw code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] students = new String[3];
+
+        students[0] = "Tommy Sawyer";
+        students[2] = "Kevin Smith";
+        students[1] = "Jeffrey Culo";
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+        for (String student : students) {
+            System.out.println(student);
+        }
     }
-} 
+    }
